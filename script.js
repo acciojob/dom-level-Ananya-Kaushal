8aedad96cb14a32751dda9c0d9a1cad2dde56e7f
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2023-10-28 18:29:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 //your JS code here. If required.
 let count=1;
-let ul=document.getElementByTagName("ul")[0];
-let i=0;
-while(i<ul.children.length)
+const level=document.getElementById("level");
+while(false)
 	{
-		let node=ul.children[i];
-		if(node.id==="level")
-		{
-			level+=1;
-		}
-		i++;
+		if(level.tagName==="HTML")break;
+		level=level.parentNode();
+		count++;
 	}
-alert("The level of the element is:"+level)
+alert("The level of the element is: "+count);
